f164adb5223be8837221ad7f616540a7d281fa53
HarisR39 <fireplatypus375@gmail.com>
2025-11-07 14:50:42 -0500

    finished interfaces

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -6,18 +6,30 @@
 
 template <typename T>
 class StackInterface {
-
+    virtual void push(const T& item) = 0;
+    virtual T pop() = 0;
+    virtual T peek() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
 
 
 template <typename T>
 class QueueInterface {
-
+    virtual void enqueue(const T& item) = 0;
+    virtual T dequeue() = 0;
+    virtual T peek() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
 
 
 template <typename T>
 class DequeInterface {
-
+    virtual void pushFront(const T& item) = 0;
+    virtual void pushBack(const T& item) = 0;
+    virtual T popFront() = 0;
+    virtual T popBack() = 0;
+    virtual const T& front() const = 0;
+    virtual const T& back() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
 
