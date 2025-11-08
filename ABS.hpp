2870f92474cda69827f142c18120bf6022ec7b81
HarisR39 <fireplatypus375@gmail.com>
2025-11-08 18:24:24 -0500

    added shrinking logic to abs

--- ABS.hpp
+++ ABS.hpp
@@ -13,17 +13,14 @@ class ABS : public StackInterface<T> {
 public:
     // Big 5 + Parameterized Constructor
     ABS() : capacity_(1), curr_size_(0){
-
         array_ = new T[capacity_];
     };
 
     explicit ABS(const size_t capacity): capacity_(capacity), curr_size_(0){
-
         array_ = new T[capacity_];
     };
 
     ABS(const ABS& other){
-
         capacity_ = other.capacity_;
         curr_size_ = other.curr_size_;
         array_ = new T[capacity_];
@@ -33,9 +30,8 @@ public:
     };
 
     ABS& operator=(const ABS& rhs){
-
         if(this == &rhs) {return *this;}
-        
+
         capacity_ = rhs.capacity_;
         curr_size_ = rhs.curr_size_;
         delete[] array_;
@@ -49,18 +45,15 @@ public:
     };
 
     ABS(ABS&& other) noexcept{
-
         capacity_ = other.capacity_;
         other.capacity_ = 0;
         curr_size_ = other.curr_size_;
         other.curr_size_ = 0;
         array_ = other.array_;
         other.array_ = nullptr;
-
     };
 
     ABS& operator=(ABS&& rhs) noexcept{
-
         if(this == &rhs) {return *this;}
 
         capacity_ = rhs.capacity_;
@@ -75,64 +68,71 @@ public:
     };
 
     ~ABS() noexcept{
-
         delete[] array_;
     };
 
     // Get the number of items in the ABS
-    [[nodiscard]] size_t getSize() const noexcept override{return curr_size_;};
+    [[nodiscard]] size_t getSize() const noexcept override { return curr_size_; };
 
     // Get the max size of the ABS
-    [[nodiscard]] size_t getMaxCapacity() const noexcept{return capacity_;};
+    [[nodiscard]] size_t getMaxCapacity() const noexcept { return capacity_; };
 
     // Return underlying data for the stack
-    [[nodiscard]] T* getData() const noexcept{return array_;};
+    [[nodiscard]] T* getData() const noexcept { return array_; };
 
     // Push item onto the stack
     void push(const T& data) override{
-
         if(curr_size_ == capacity_){
-
             capacity_ *= scale_factor_;
             T* temp = new T[capacity_];
             for(size_t i = 0; i < curr_size_; ++i){
-
                 temp[i] = array_[i];
             }
-
             delete[] array_;
             array_ = temp;
         }
 
         array_[curr_size_] = data;
-        curr_size_ ++;
+        curr_size_++;
     };
 
     T peek() const override{
-
-        if(curr_size_ == 0){throw std::runtime_error("The stack is empty");}
+        if(curr_size_ == 0){ throw std::runtime_error("The stack is empty"); }
         return array_[curr_size_ - 1];
     };
 
     T pop() override{
+        if(curr_size_ == 0){ throw std::runtime_error("The stack is empty"); }
 
-        if(curr_size_ == 0){throw std::runtime_error("The stack is empty");}
         curr_size_--;
-        return array_[curr_size_];
+        T item = array_[curr_size_];
+
+        // ✅ shrink capacity if too empty
+        if(curr_size_ <= capacity_ / 4 && capacity_ > 1){
+            size_t new_capacity = capacity_ / 2;
+            if(new_capacity < 1) { new_capacity = 1; }
+
+            T* temp = new T[new_capacity];
+            for(size_t i = 0; i < curr_size_; ++i){
+                temp[i] = array_[i];
+            }
+
+            delete[] array_;
+            array_ = temp;
+            capacity_ = new_capacity;
+        }
+
+        return item;
     };
 
     void PrintForward(){
-
         for(size_t i = 0; i < curr_size_; ++i){
-
             std::cout << array_[i] << std::endl;
         }
     }
 
     void PrintReverse(){
-
         for(size_t i = curr_size_; i <= 0; --i){
-
             std::cout << array_[i] << std::endl;
         }
     }
