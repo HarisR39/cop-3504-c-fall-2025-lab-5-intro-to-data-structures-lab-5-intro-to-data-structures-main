683c5a0c43d1afa0b13162ffceb7a5d021763647
HarisR39 <fireplatypus375@gmail.com>
2025-11-08 13:05:39 -0500

    finished llq and lldq

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,40 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() = default;
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override{
+
+        list.addHead(item);
+    };
+
+    void pushBack(const T& item) override{
+
+        list.addTail(item);
+    };
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override{
+
+        Node<T>* temp = list.getHead();
+        list.removeHead();
+        return temp -> data;
+    };
+
+    T popBack() override{
+
+        Node<T>* temp = list.getTail();
+        list.removeTail();
+        return temp -> data;
+    };
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override{}{return (list.getHead()) -> data;};
+    const T& back() const override{return (list.getTail()) -> data;};
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{return list.getCount();};
 };
 
 
--- LLQ.hpp
+++ LLQ.hpp
@@ -11,18 +11,26 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLQ();
+    LLQ() = default;
 
     // Insertion
-    void enqueue(const T& item) override;
+    void enqueue(const T& item) override{
+
+        list.addTail(item);
+    };
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override{
+
+        Node<T>* temp = list.getHead();
+        list.removeHead();
+        return temp -> data;
+    };
 
     // Access
-    T peek() const override;
+    T peek() const override {return (list.getHead()) -> data;};
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{return list.getCount();};
 
 };
